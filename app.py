7586628dff45b56c1c269ab58c4667555a2cde80
Tony Zhao <tonyzhao668@yahoo.com.au>
2021-05-15 16:12:42 +0800

    DATABASE_URL added

--- app.py
+++ app.py
@@ -18,7 +18,8 @@ app = Flask(__name__)
 #################################################
 
 from flask_sqlalchemy import SQLAlchemy
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '') or "sqlite:///db.sqlite"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', '')
+# ('DATABASE_URL', '') or "sqlite:///db.sqlite"
 
 # Remove tracking modifications
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
